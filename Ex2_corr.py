56d6010874170af7ac7dc5e45785358a63df3c80
shaghayeghmazaheri <mazaheri446@gmail.com>
2023-06-04 19:21:14 +0200

    test

---
+++ Ex2_corr.py
@@ -0,0 +1,11 @@
+n= int(input('primefactors.py '))
+print(n, ' = ' , end='')
+
+for i in range(2,n+1):
+    if(n%i) == 0:
+        while(n%i ==0 ):
+            n = n/i
+            print(i , end='')
+            if(n!=1):
+                print(' * ',end='')
+# slower than the suggested algorithm
